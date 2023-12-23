be1e0c52f55bcb1ebfb2c57e56fe41b610dcb8af
Jonah <shablargablargn@New-PC>
2023-12-22 19:43:47 -0600

    starting code, files created

---
+++ index.js
@@ -0,0 +1,18 @@
+<div class="container">
+    <div id="listHead" class="header"> 
+        <h2>To Do List</h2>
+        <input type="text" id="myInput" placeholder="Title..."/>
+        <span onClick="newElement()" class="addButton">Add</span>
+    </div>
+
+    <div class="content">
+        <ul>
+            <li>Go to gym</li>
+            <li class="done">Groceries</li>
+        </ul>
+    
+        <div class="addItem">
+
+        </div>
+    </div>
+</div>
